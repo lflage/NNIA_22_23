df0afc38b19713626348e680ee6976ca1772802b
Lucas Lage <lucasflage@hotmail.com>
2022-11-13 22:35:39 +0100

    Finished untill exercise 4.2

--- Assignment_1/my_func.py
+++ Assignment_1/my_func.py
@@ -1,2 +1,15 @@
 def my_print():
     print("Hello Tim!!")
+
+def my_mean(a):
+    # using python built in functions to calculate the mean
+    # I could also iterate over the np.array and sum the items
+    # but its ineficcient
+    return sum(a)/len(a)
+
+def my_std(a):
+    mean = my_mean(a)
+    b = 0
+    for i in a:
+        b += (i - mean)**2
+    return (b / len(a - 1))**(1/2)
